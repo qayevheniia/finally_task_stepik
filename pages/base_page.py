bb4b6228ecc053e2dda3071ff62862ae608ab8bb
qayevheniia <61921504+qayevheniia@users.noreply.github.com>
2020-06-03 00:49:38 +0300

    create locators.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,4 @@
-from selenium.common.exceptions import имя_исключения
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     def __init__(self, browser, url):
@@ -12,3 +12,13 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
