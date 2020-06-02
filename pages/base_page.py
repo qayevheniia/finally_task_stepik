3fdeb9bfe7fe5dd1816610bfa5e13932acd050ba
qayevheniia <61921504+qayevheniia@users.noreply.github.com>
2020-06-02 23:03:56 +0300

    Проверка main page на присутствие

---
+++ pages/base_page.py
@@ -0,0 +1,14 @@
+from selenium.common.exceptions import имя_исключения
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
