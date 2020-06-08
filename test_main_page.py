b4ab562195cca2bb782d77f4dc48b364943b69ce
qayevheniia <61921504+qayevheniia@users.noreply.github.com>
2020-06-08 10:46:49 +0300

    Checks several link in the same time and xfail on of them

--- test_main_page.py
+++ test_main_page.py
@@ -30,9 +30,11 @@ def test_guest_should_be_login_form(browser):
     page.open()
     page.should_be_login_form()
 
-@pytest.mark.last_test
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019."
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     page = ProductPage(browser,link)
     page.open()
     page.can_add_product_to_basket ()
